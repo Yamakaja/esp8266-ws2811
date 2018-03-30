a14195584cadd213b7fc845269f72d587f0e93e0
Yamakaja <dastw@gmx.net>
2018-03-30 14:02:12 +0200

    Cleanup

--- client.py
+++ client.py
@@ -7,13 +7,17 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 def getAll(r, g, b):
     result = bytes(0)
     for i in range(19):
-        result += bytes((g, r, b))
-    result += b'\xFE'
+        result += bytes((r, g, b))
     return result
 
-while True:
-    input()
-    s.sendto(getAll(255, 0, 0), ("192.168.2.107", 1234))
-    input()
-    s.sendto(getAll(0, 0, 0), ("192.168.2.107", 1234))
+s.sendto(getAll(0, 255, 0), ("192.168.2.107", 1234))
+
+# while True:
+#     for i in range(255):
+#         s.sendto(getAll(0, i, 0), ("192.168.2.107", 1234))
+#         time.sleep(0.05)
+#         print(time.time())
+#     for i in range(255):
+#         s.sendto(getAll(0, 255-i, 0), ("192.168.2.107", 1234))
+#         time.sleep(0.05)
 
